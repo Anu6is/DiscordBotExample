d47c5672a08f5751568cbc23eae7b027ff80614f
Anu6is <curtis.mayers@gmail.com>
2018-03-06 11:41:04 -0500

    Basic bot template

---
+++ DiscordBot/Modules/SimpleExample.vb
@@ -0,0 +1,39 @@
+﻿Imports Discord.Commands
+Imports Discord.WebSocket
+
+Namespace Modules
+    Public Class SimpleExample
+        Inherits ModuleBase(Of SocketCommandContext)
+        'In order to trigger a command, the user must type the prefix expected by the command handler, followed by the command name or alias. 
+
+        <Command("hi")> 'To execute this command the user needs to enter |>hi
+        <[Alias]("hello")> 'alternately, they can use |>hello
+        <Summary("Replies to the user that typed the command")>
+        Public Function SayAsync() As Task
+            Return ReplyAsync($"Hello {Context.User.Mention}")
+        End Function
+
+        '|>say text for the bot to repeat
+        'To accept user input with the command, the method signature must include parameters to capture the user input
+        <Command("say")>
+        <Summary("Responds with whatever the user enters after the command trigger")>
+        <Remarks("The Remainder attribute is useful for accepting multi-word inputs. Without it, a sentence would have to be wrapped in quotes.")>
+        Public Function EchoAsync(<Remainder> ByVal text As String) As Task
+            Return ReplyAsync(text)
+        End Function
+
+        '|>userinfo @Anu6is
+        <Command("userinfo")>
+        <[Alias]("whois")>
+        <Summary("Displays basic user info")>
+        <Remarks("Discord.Net provides various TypeReaders that you can utilize to assist with parsing commands. Example: SocketGuildUser")>
+        Public Function UserInfo(Optional ByVal user As SocketGuildUser = Nothing) As Task
+            'Context provides access to a host of properties made available from the DiscordSocketClient as the SocketUserMessage
+            If user Is Nothing Then user = Context.User 'The user that typed the command
+
+            'An alternate way to send a message. Sending a message simply requires an implementation of ITextChannel
+            Return Context.Channel.SendMessageAsync($"{user.Username}#{user.Discriminator} joined **{Context.Guild.Name}** on *{user.JoinedAt.Value.ToString("f")}*")
+        End Function
+    End Class
+End Namespace
+
--- DiscordBot/Program.vb
+++ DiscordBot/Program.vb
@@ -1,7 +1,122 @@
-Imports System
+
+Imports System.IO
+Imports System.Reflection
+Imports System.Threading
+Imports Discord
+Imports Discord.Commands
+Imports Discord.WebSocket
+Imports Microsoft.Extensions.Configuration          'Required for ConfigurationBuilder
+Imports Microsoft.Extensions.DependencyInjection    'Required for ServiceCollection
 
 Module Program
-    Sub Main(args As String())
-        Console.WriteLine("Hello World!")
+    Private _client As DiscordSocketClient
+    Private _commands As CommandService
+    Private _config As IConfiguration
+    Private _services As IServiceProvider
+
+    Sub Main()
+        'Call the Start function and wait until it completes (which should be never)
+        Start().GetAwaiter().GetResult()
     End Sub
+
+    Async Function Start() As Task
+        _client = New DiscordSocketClient()
+        _commands = New CommandService()
+        _config = BuildConfig()
+
+        'Set up your Dependency Injection Container
+        Await AddServices()
+
+        'Subscribe to desired events
+        Await AttachHandlers()
+
+        'Load commands and modules into the command service
+        Await _commands.AddModulesAsync(Assembly.GetEntryAssembly())
+
+        Await _client.LoginAsync(TokenType.Bot, _config("token")) 'A valid token must exist in config.json
+        Await _client.StartAsync()
+
+        'Infinite delay; Keeps the console open and the bot connected
+        Await Task.Delay(Timeout.Infinite)
+    End Function
+
+    Private Function AddServices() As Task
+        Dim collection As New ServiceCollection()
+
+        'Basic services
+        collection.AddSingleton(_client)
+        collection.AddSingleton(_commands)
+
+        'Additional services and dependencies that you require
+        collection.AddSingleton(_config)
+
+        'All additional services should be added before building
+
+        _services = collection.BuildServiceProvider()
+
+        Return Task.CompletedTask
+    End Function
+
+    Private Function AttachHandlers() As Task
+        AddHandler _client.Log, AddressOf Logger
+        AddHandler _commands.Log, AddressOf Logger
+
+        AddHandler _client.MessageReceived, AddressOf CommandHandler
+
+        Return Task.CompletedTask
+    End Function
+
+    Private Async Function CommandHandler(ByVal message As SocketMessage) As Task
+        Dim userMessage As SocketUserMessage = TryCast(message, SocketUserMessage)
+
+        'Ignore non user messages 
+        If userMessage Is Nothing OrElse userMessage.Author.IsBot Then Return
+
+        Dim pos As Integer = 0
+
+        'Create the command context
+        Dim context As New SocketCommandContext(_client, userMessage)
+
+        'This is where you set your command prefix. This can be hard coded or made configurable.
+        'HasCharPrefix - checks to see if the message begins with a predefined Character
+        'HasStringPrefix - checks to see if the message begins with a predefined String
+        'HasMentionPrefix - checks to see if the message begins with a predefined user Mention
+        If userMessage.HasStringPrefix("|>", pos) OrElse userMessage.HasMentionPrefix(_client.CurrentUser, pos) Then
+            Dim result As IResult = Await _commands.ExecuteAsync(context, pos, _services)
+
+            'Send a message if the command failed
+            If Not result.IsSuccess AndAlso Not result.ErrorReason = CommandError.UnknownCommand Then
+                Await userMessage.Channel.SendMessageAsync(result.ErrorReason)
+            End If
+
+            'If you so desire, you can configure behaviour specific to the error type
+            'Remove the lines above and uncomment the line below
+            'If Not result.IsSuccess Then
+            '    Select Case result.GetType
+            '        Case GetType(PreconditionResult)
+            '            'Do something
+            '        Case GetType(PreconditionGroupResult)
+            '            'Do something 
+            '        Case GetType(ParseResult)
+            '            'Do something
+            '        Case GetType(SearchResult)
+            '            'Do something
+            '        Case GetType(TypeReaderResult)
+            '            'Do something
+            '        Case Else
+            '            'Do something
+            '    End Select
+            'End If
+        End If
+    End Function
+
+    Private Function Logger(ByVal message As LogMessage, Optional task As Task = Nothing) As Task
+        'Very basic logging
+        Console.WriteLine($"{DateTime.Now,-19} [{message.Severity,8}] {message.Source}: {message.Message} {message.Exception}")
+        Return Task.CompletedTask
+    End Function
+
+    Private Function BuildConfig() As IConfiguration
+        Return New ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory).AddJsonFile("config.json").Build
+    End Function
 End Module
