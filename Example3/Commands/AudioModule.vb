b4d6a9b8230ce7270e276e45939873b829b17a60
Anu6is <curtis.mayers@gmail.com>
2020-05-13 07:26:26 -0400

    Added voice recording

--- Example3/Commands/AudioModule.vb
+++ Example3/Commands/AudioModule.vb
@@ -17,21 +17,33 @@ Public Class AudioModule
     ''' </summary>
     <Command("join", RunMode:=RunMode.Async)>
     <Summary("Attempts to have the bot join the voice channel the user is connected to")>
-    Public Async Function JoinCmd() As Task
+    Public Async Function JoinAsync() As Task
         Dim voiceState = TryCast(Context.User, IVoiceState)
 
-        If voiceState Is Nothing Then Await ReplyAsync("You must be connected to a voice channel!") : Return
+        If voiceState.VoiceChannel Is Nothing Then Await ReplyAsync("You must be connected to a voice channel!") : Return
 
-        Await _service.JoinAudio(Context.Guild, voiceState.VoiceChannel)
+        Await _service.JoinAudioAsync(Context.Guild, voiceState.VoiceChannel)
     End Function
 
     <Command("leave", RunMode:=RunMode.Async)>
-    Public Async Function LeaveCmd() As Task
-        Await _service.LeaveAudio(Context.Guild)
+    <Summary("Disconnects the bot from the voice channel")>
+    Public Async Function LeaveAsync() As Task
+        Await _service.LeaveAudioAsync(Context.Guild)
     End Function
 
     <Command("play", RunMode:=RunMode.Async)>
-    Public Async Function PlayCmd(<Remainder> song As String) As Task
-        Await _service.SendAudioAsync(Context.Guild, Context.Channel, song)
+    <Summary("Plays a local audio file")>
+    Public Async Function PlayAsync(<Remainder> filepath As String) As Task
+        Await _service.SendAudioAsync(Context.Guild, Context.Channel, filepath)
+    End Function
+
+    <Command("record", RunMode:=RunMode.Async)>
+    <Summary("Records the audio stream of each user in the voice channel")>
+    Public Async Function RecordAsync() As Task
+        Dim voiceState = TryCast(Context.User, IVoiceState)
+
+        If voiceState.VoiceChannel Is Nothing Then Await ReplyAsync("You must be connected to a voice channel!") : Return
+
+        Await _service.RecordAudioAsync(Context.Guild, Context.Channel, Context.User)
     End Function
 End Class
--- Example3/Services/AudioService.vb
+++ Example3/Services/AudioService.vb
@@ -1,25 +1,49 @@
 ﻿Imports System.Collections.Concurrent
 Imports System.IO
+Imports System.Threading
 Imports Discord
 Imports Discord.Audio
+Imports Discord.WebSocket
 
+''' <summary>
+'''     This is a basic implementation of a bot sending and receiving audio.
+''' </summary>
 Public Class AudioService
-    Private ReadOnly ConnectedChannels As ConcurrentDictionary(Of ULong, IAudioClient) = New ConcurrentDictionary(Of ULong, IAudioClient)()
+    Private ReadOnly ConnectedChannels As New ConcurrentDictionary(Of ULong, IAudioClient)
+    Private ReadOnly RecordingUsers As New ConcurrentDictionary(Of ULong, AudioInStream)
+    Private ReadOnly Client As DiscordSocketClient
 
-    Public Async Function JoinAudio(guild As IGuild, target As IVoiceChannel) As Task
+    Public Sub New(client As DiscordSocketClient)
+        Me.Client = client
+        AddHandler client.UserVoiceStateUpdated, AddressOf VoiceUserDisconnectedAsync
+    End Sub
+
+    ''' <summary>
+    '''     <see cref="IVoiceChannel.ConnectAsync()"/> returns the <see cref="IAudioClient"/>
+    '''     used for sending audio
+    ''' </summary>
+    Public Async Function JoinAudioAsync(guild As IGuild, voiceChannel As IVoiceChannel) As Task
         Dim client As IAudioClient = Nothing
 
         If ConnectedChannels.TryGetValue(guild.Id, client) Then Return 'Already connected
 
-        Dim audioClient = Await target.ConnectAsync()
+        Dim audioClient = Await voiceChannel.ConnectAsync()
 
         If ConnectedChannels.TryAdd(guild.Id, audioClient) Then
             Console.WriteLine($"Connected to voice in {guild.Name}")
         End If
     End Function
 
-    Public Async Function LeaveAudio(ByVal guild As IGuild) As Task
+    ''' <summary>
+    '''     To disconnect from a voice channel, call <see cref="IAudioClient.StopAsync()"/>
+    '''     Before disconnecting, stop any voice recordings by simply removing the user from <see cref="RecordingUsers"/>
+    ''' </summary>
+    Public Async Function LeaveAudioAsync(guild As SocketGuild) As Task
         Dim client As IAudioClient = Nothing
+
+        For Each user In guild.CurrentUser.VoiceChannel.Users
+            If RecordingUsers.ContainsKey(user.Id) Then RecordingUsers.TryRemove(user.Id, Nothing)
+        Next
 
         If ConnectedChannels.TryRemove(guild.Id, client) Then
             Await client.StopAsync()
@@ -27,7 +51,7 @@ Public Class AudioService
         End If
     End Function
 
-    Public Async Function SendAudioAsync(ByVal guild As IGuild, ByVal channel As IMessageChannel, ByVal path As String) As Task
+    Public Async Function SendAudioAsync(guild As IGuild, channel As IMessageChannel, path As String) As Task
         'NOTE: You need a full path to the file if the value of 'path' is only a filename.
 
         If Not File.Exists(path) Then path = $".\{path}"
@@ -37,26 +61,111 @@ Public Class AudioService
 
         If ConnectedChannels.TryGetValue(guild.Id, client) Then
             Console.WriteLine($"Starting playback of {path} in {guild.Name}")
-            Using ffmpeg = CreateProcess(path)
+            Using ffmpeg = CreateOutputProcess(path)
                 Using stream = client.CreatePCMStream(AudioApplication.Music)
                     Try
                         Await ffmpeg.StandardOutput.BaseStream.CopyToAsync(stream)
                     Catch e As Exception
                         Console.WriteLine(e.ToString)
                     End Try
-
                     Await stream.FlushAsync()
                 End Using
             End Using
+            Console.WriteLine($"{path} ended in {guild.Name}")
         End If
-        End Function
+    End Function
 
-    Private Function CreateProcess(ByVal path As String) As Process
+    Public Async Function RecordAudioAsync(guild As SocketGuild, channel As IMessageChannel, user As SocketGuildUser) As Task
+        Dim client As IAudioClient = Nothing
+
+        If RecordingUsers.TryGetValue(user.Id, client) Then Return 'Already recording
+        If Not ConnectedChannels.TryGetValue(guild.Id, client) Then Await channel.SendMessageAsync("Run the `join` command") : Return
+
+        'Discord requires that audio be sent to the channel before you can start receiving voice
+        'Send short audio notification to establish listening rights
+        'This could be done immediately after joining the channel
+        Await SendAudioAsync(guild, channel, "./recording_audio.mp3")
+
+        For Each voiceUser In user.VoiceChannel.Users
+            If guild.CurrentUser.Id = voiceUser.Id Then Continue For
+            Dim tsk = ListenToStreamAsync(voiceUser.Id, voiceUser.AudioStream) 'listen to current users of the channel
+        Next
+
+        'StreamCreated is triggered when a user joins a voice channel
+        AddHandler client.StreamCreated, AddressOf ListenToStreamAsync 'listen to users joining the channel
+        AddHandler client.StreamDestroyed, AddressOf StreamDestroyedAsync
+    End Function
+
+    Private Function ListenToStreamAsync(id As ULong, inStream As AudioInStream) As Task
+        RecordingUsers.TryAdd(id, inStream)
+        Return CaptureAudioAsync(id, inStream)
+    End Function
+
+    Private Function StreamDestroyedAsync(id As ULong) As Task
+        RecordingUsers.TryRemove(id, Nothing)
+        Return Task.CompletedTask
+    End Function
+
+    ''' <summary>
+    '''     Stop recording a user when they leave the voice channel
+    ''' </summary>
+    Private Function VoiceUserDisconnectedAsync(user As SocketUser, before As SocketVoiceState, after As SocketVoiceState) As Task
+        If RecordingUsers.ContainsKey(user.Id) AndAlso after.VoiceChannel Is Nothing Then
+            RecordingUsers.TryRemove(user.Id, Nothing)
+        End If
+        Return Task.CompletedTask
+    End Function
+
+    Public Async Function CaptureAudioAsync(id As ULong, inStream As AudioInStream) As Task
+        Dim cts = New CancellationTokenSource()
+        Dim path = Client.GetUser(id).ToString & DateTimeOffset.UtcNow.ToUnixTimeMilliseconds
+
+        Console.WriteLine($"Start recording {Client.GetUser(id)}")
+        Using ffmpeg = CreateInputProces(path)
+            Using stream = ffmpeg.StandardInput.BaseStream
+                Try
+                    While RecordingUsers.ContainsKey(id) 'If the key (user id) is removed, recording is stopped
+                        If inStream.AvailableFrames > 0 Then
+                            Dim rtpFrame As RTPFrame = Nothing
+                            Dim readFrame = inStream.TryReadFrame(cts.Token, rtpFrame)
+
+                            Await stream.WriteAsync(rtpFrame.Payload, 0, rtpFrame.Payload.Length)
+                            Await stream.FlushAsync()
+                        End If
+                    End While
+                Catch ex As Exception
+                    Console.WriteLine(ex.ToString)
+                End Try
+                cts.Dispose()
+                Await inStream.DisposeAsync()
+                Console.WriteLine($"Stop recording {Client.GetUser(id)}")
+            End Using
+        End Using
+    End Function
+
+    ''' <summary>
+    '''     FFMPEG process for playing an audio file
+    ''' </summary>
+    ''' <param name="path">The filepath for the audio file to be played</param>
+    Private Function CreateOutputProcess(path As String) As Process
         Return Process.Start(New ProcessStartInfo With {
             .FileName = "ffmpeg.exe",
             .Arguments = $"-hide_banner -loglevel panic -i ""{path}"" -ac 2 -f s16le -ar 48000 pipe:1",
             .UseShellExecute = False,
             .RedirectStandardOutput = True
+        })
+    End Function
+
+    ''' <summary>
+    '''     FFMPEG process for recording an audio file
+    ''' </summary>
+    ''' <param name="path">The filepath for storing the recording</param>
+    Private Function CreateInputProces(path As String) As Process
+        Return Process.Start(New ProcessStartInfo With {
+            .FileName = "ffmpeg.exe",
+            .Arguments = $"-hide_banner -loglevel panic -ac 2 -f s16le -ar 48000 -i pipe:0 -ar 44100 .\{path}.wav",
+            .UseShellExecute = False,
+            .RedirectStandardInput = True
         })
     End Function
 End Class
